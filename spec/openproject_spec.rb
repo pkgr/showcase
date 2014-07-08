36c4ffbf22f550f7c4391656fbd99b455451e849
Cyril Rohr <cyril.rohr@gmail.com>
2014-07-08 22:23:11 +0100

    Add tests for OpenProject.

---
+++ spec/openproject_spec.rb
@@ -0,0 +1,34 @@
+require File.expand_path('../spec_helper', __FILE__)
+
+describe "OpenProject" do
+  def launch_test(distribution, command)
+    Instance.launch(distribution) do |instance|
+      instance.ssh(command) do |ssh|
+        wait_until { ssh.exec!("ps -u openproject -f").include?("unicorn") }
+
+        ps_output = ssh.exec!("ps -u openproject -f")
+        expect(ps_output).to include("unicorn")
+
+        visit "http://#{instance.hostname}"
+        expect(page).to have_content("Sign in")
+      end
+    end
+  end
+
+  context "packaging branch" do
+    [
+      "debian-7"
+    ].map{|d| Distribution.new(d) }.each do |distribution|
+      it "deploys OpenProject on #{distribution.name}" do
+        template = Template.new(
+          data_file("openproject.sh.erb"),
+          codename: distribution.codename,
+          branch: "packaging",
+          repo_url: "https://deb.packager.io/gh/crohr/openproject"
+        )
+        command = Command.new(template.render, sudo: true)
+        launch_test(distribution, command)
+      end
+    end
+  end
+end
