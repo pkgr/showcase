b91d230dcebb172067e5e3d92f0b7f8409482262
Cyril Rohr <cyril.rohr@gmail.com>
2014-07-28 13:58:35 +0000

    Switch back to packaging branch. Add admin_email

--- spec/openproject_spec.rb
+++ spec/openproject_spec.rb
@@ -23,7 +23,7 @@ describe "OpenProject" do
         template = Template.new(
           data_file("openproject.sh.erb"),
           codename: distribution.codename,
-          branch: "packaging-svn-integration",
+          branch: "packaging",
           repo_url: "https://deb.packager.io/gh/crohr/openproject"
         )
         command = Command.new(template.render, sudo: true)
