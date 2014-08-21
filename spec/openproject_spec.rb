90aba793bd4eca3afb6d11c75797350a5744652a
Cyril Rohr <cyril.rohr@gmail.com>
2014-08-21 09:27:51 +0000

    Parametrize app name for openproject

--- spec/openproject_spec.rb
+++ spec/openproject_spec.rb
@@ -1,19 +1,33 @@
 require File.expand_path('../spec_helper', __FILE__)
 
 describe "OpenProject" do
+  def app_name
+    ENV.fetch('OPENPROJECT_APP_NAME') { "openproject" }
+  end
+
+  def branch
+    ENV.fetch('OPENPROJECT_BRANCH') { "packaging" }
+  end
+
+  def repo_url
+    ENV.fetch('OPENPROJECT_REPO_URL') { "https://deb.packager.io/gh/crohr/openproject" }
+  end
+
   def launch_test(distribution, command, tag_val = nil)
     Instance.launch(distribution, tag_val) do |instance|
       instance.ssh(command) do |ssh|
+        url = "https://#{instance.hostname}"
+        puts url
         wait_until { ssh.exec!("ps -u someuser -f").include?("unicorn worker") }
 
         ps_output = ssh.exec!("ps -u someuser -f")
         expect(ps_output).to include("unicorn")
 
-        check_output = ssh.exec!("sudo openproject run check")
+        check_output = ssh.exec!("sudo #{app_name} run check")
         puts check_output
         expect(check_output).to_not include("[ko]")
 
-        visit "https://#{instance.hostname}"
+        visit url
         expect(page).to have_content("Sign in")
         click_on "Sign in"
         fill_in "Login", with: "admin"
@@ -36,8 +50,9 @@ describe "OpenProject" do
         template = Template.new(
           data_file("openproject-new-database.sh.erb"),
           codename: distribution.codename,
-          branch: "packaging",
-          repo_url: "https://deb.packager.io/gh/crohr/openproject"
+          branch: branch,
+          repo_url: repo_url,
+          app_name: app_name
         )
         command = Command.new(template.render, sudo: true)
         launch_test(distribution, command, "#{distribution.name} - openproject new database")
@@ -47,8 +62,9 @@ describe "OpenProject" do
         template = Template.new(
           data_file("openproject-existing-database.sh.erb"),
           codename: distribution.codename,
-          branch: "packaging",
-          repo_url: "https://deb.packager.io/gh/crohr/openproject"
+          branch: branch,
+          repo_url: repo_url,
+          app_name: app_name
         )
         command = Command.new(template.render, sudo: true)
         launch_test(distribution, command, "#{distribution.name} - openproject existing database")
