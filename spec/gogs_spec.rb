3ad2a91deed3b734c1019c44c8f94667deb309bf
Cyril Rohr <cyril.rohr@gmail.com>
2015-01-06 11:30:19 +0000

    Fix gogs

--- spec/gogs_spec.rb
+++ spec/gogs_spec.rb
@@ -8,63 +8,68 @@ describe "Gogs" do
   def launch_test(distribution, command)
     Instance.launch(distribution) do |instance|
       instance.ssh(command) do |ssh|
-        puts "http://#{instance.hostname}"
-        wait_until { ssh.exec!("ps -u gogs -f").include?("web") }
+        begin
+          puts "http://#{instance.hostname}"
+          wait_until { ssh.exec!("ps -u gogs -f").include?("web") }
 
-        visit "http://#{instance.hostname}"
+          visit "http://#{instance.hostname}"
 
-        unless page.body.include?("Sign In")
-          expect(page).to have_content("Install")
-          fill_in "passwd", with: "p4ssw0rd"
-          fill_in "Application URL", with: "http://#{instance.hostname}"
-          fill_in "Domain", with: instance.hostname
-          fill_in "Username", with: "crohr"
-          fill_in "admin_pwd", with: "p4ssw0rd"
-          fill_in "Confirm Password", with: "p4ssw0rd"
-          fill_in "E-mail", with: "cyril.rohr@gmail.com"
-          click_button "Install Gogs"
+          unless page.body.include?("Sign In")
+            expect(page).to have_content("Install")
+            fill_in "passwd", with: "p4ssw0rd"
+            fill_in "Application URL", with: "http://#{instance.hostname}"
+            fill_in "Domain", with: instance.hostname
+            fill_in "Username", with: "crohr"
+            fill_in "admin_pwd", with: "p4ssw0rd"
+            fill_in "Confirm Password", with: "p4ssw0rd"
+            fill_in "E-mail", with: "cyril.rohr@gmail.com"
+            click_button "Install Gogs"
+          end
+
+          rand = Time.now.to_i
+
+          expect(page).to have_button("Sign In")
+          fill_in "username", with: "crohr"
+          fill_in "password", with: "p4ssw0rd"
+          click_button "Sign In"
+          # Gogs is not yet QA friendly...
+          visit "http://#{instance.hostname}/repo/create"
+          expect(page).to have_content("Repository Name")
+          fill_in "repo_name", with: "hello-#{rand}"
+          check "private"
+          click_button "Create Repository"
+
+          clone_url = "gogs@#{instance.hostname}:crohr/hello-#{rand}.git"
+          expect(page).to have_content("Clone this repository")
+          expect(page).to have_content(clone_url)
+
+          click_on "Account Settings"
+          click_link "SSH Keys"
+          click_on "Add Key"
+          fill_in "Key Name", with: "test"
+          fill_in "Content", with: File.read(fixture("id_rsa.pub"))
+          click_button "ssh-add-btn"
+          expect(page).to have_content("New SSH Key has been added")
+
+          system(%{
+  ssh-keyscan #{instance.hostname} >> ~/.ssh/known_hosts ; \
+  ssh-agent bash -c " \
+    ssh-add #{fixture("id_rsa")}; \
+    cd /tmp && \
+    git clone #{clone_url} && \
+    cd hello-#{rand} && \
+    echo world > README.md && \
+    git add README.md && \
+    git commit -am 'commit message' && \
+    git push origin master"
+  })
+
+          click_link "Dashboard"
+          expect(page).to have_content("commit message")
+        rescue
+          puts ssh.exec!("sudo gogs logs")
+          raise
         end
-
-        rand = Time.now.to_i
-
-        expect(page).to have_button("Sign In")
-        fill_in "username", with: "crohr"
-        fill_in "password", with: "p4ssw0rd"
-        click_button "Sign In"
-        # Gogs is not yet QA friendly...
-        visit "http://#{instance.hostname}/repo/create"
-        expect(page).to have_content("Repository Name")
-        fill_in "repo_name", with: "hello-#{rand}"
-        check "private"
-        click_button "Create Repository"
-
-        clone_url = "gogs@#{instance.hostname}:crohr/hello-#{rand}.git"
-        expect(page).to have_content("Clone this repository")
-        expect(page).to have_content(clone_url)
-
-        click_on "Account Settings"
-        click_link "SSH Keys"
-        click_on "Add Key"
-        fill_in "Key Name", with: "test"
-        fill_in "Content", with: File.read(fixture("id_rsa.pub"))
-        click_button "ssh-add-btn"
-        expect(page).to have_content("New SSH Key has been added")
-
-        system(%{
-ssh-keyscan #{instance.hostname} >> ~/.ssh/known_hosts ; \
-ssh-agent bash -c " \
-  ssh-add #{fixture("id_rsa")}; \
-  cd /tmp && \
-  git clone #{clone_url} && \
-  cd hello-#{rand} && \
-  echo world > README.md && \
-  git add README.md && \
-  git commit -am 'commit message' && \
-  git push origin master"
-})
-
-        click_link "Dashboard"
-        expect(page).to have_content("commit message")
       end
     end
   end
